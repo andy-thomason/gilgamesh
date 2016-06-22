d11877a34f0672234a5ee151759c7298ab42a4ee
andy-thomason <andy@andythomason.com>
2016-06-23 00:35:22 +0100

    more kd tree

--- examples/molecules/main.cpp
+++ examples/molecules/main.cpp
@@ -16,15 +16,17 @@ inline std::ostream & operator<<(std::ostream &os, const glm::vec3 &v) {
 class kdtree {
 public:
   kdtree(const std::vector<glm::vec3> &pos) {
+    std::vector<sorter_t> sorter;
+    sorter.reserve(pos.size());
+    
     for (auto &p : pos) {
-      sorter_.emplace_back(p, &p - pos.data());
+      sorter.emplace_back(p, &p - pos.data());
     }
 
-    size_t size = sorter_.size();
-    auto *begin = sorter_.data();
+    size_t size = sorter.size();
+    auto *begin = sorter.data();
     auto *end = begin + size;
 
-    int level = 0;
     int axis = 0;
     size_t step = size;
     for (;;) {
@@ -42,41 +44,54 @@ public:
         }
       );
 
-      /*printf("---\n");
-      for (auto *p = begin; p != end; ++p) {
-        printf("%5d %5d %f %f %f %d\n", p - begin, p->partition, p->pos.x, p->pos.y, p->pos.z, p->index);
-      }*/
+      if (step == 1) {
+        printf("---\n");
+        for (auto *p = begin; p != end; ++p) {
+          printf("%5d %5d %f %f %f %d\n", (int)(p - begin), (int)(p->partition), p->pos.x, p->pos.y, p->pos.z, (int)p->index);
+        }
+      }
+
 
       if (step == 1) break;
+      
+      for (size_t lower = 0; lower < size; lower += step) {
+        size_t upper = std::min(lower + step, size);
+        size_t mid = lower + (step + 1) / 2;
+        partition_t p = { lower, upper, mid, begin[mid].pos[axis] };
+        printf("p%d %d %d %d %f\n", (int)partitions_.size(), (int)lower, (int)mid, (int)upper, begin[mid].pos[axis]);
+        partitions_.push_back(p);
+      }
       axis = axis == 2 ? 0 : axis + 1;
       step = (step + 1) / 2;
     }
 
     in_starts_.reserve(size);
     out_starts_.reserve(size);
+    size_ = size;
   }
 
   // note that this is not thread safe
   const std::vector <size_t> &find_in_range(const glm::vec3 &min, const glm::vec3 &max) {
-    int level = 0;
     int axis = 0;
-    size_t size = sorter_.size();
-    size_t step = size;
+    size_t step = size_;
     in_starts_.resize(0);
     in_starts_.emplace_back(0);
     for (;;) {
       out_starts_.resize(0);
       float minx = min[axis];
       float maxx = max[axis];
-      printf("l=%d %f..%f\n", level, minx, maxx);
-      for (size_t lower : in_starts_) {
-        size_t mid = lower + (step + 1) / 2;
-        size_t upper = std::min(lower + step, size);
-        printf("%d..%d..%d %f..%f %f..%f\n", lower, mid, upper, sorter_[lower].pos[axis], sorter_[mid-1].pos[axis], sorter_[mid].pos[axis], sorter_[upper-1].pos[axis]);
-        if (maxx >= sorter_[lower].pos[axis] && minx <= sorter_[mid-1].pos[axis]) {
+      printf("s=%d %f..%f\n", (int)step, minx, maxx);
+      for (size_t idx : in_starts_) {
+        partition_t p = partitions_[idx];
+        size_t lower = p.lower;
+        size_t upper = p.upper;
+        size_t mid = p.mid;
+        float mid_value = p.mid_value;
+        printf("%d..%d..%d mid_value=%f\n", (int)lower, (int)mid, (int)upper, mid_value);
+        if (minx <= mid_value) {
           out_starts_.emplace_back(lower);
         }
-        if (maxx >= sorter_[mid].pos[axis] && minx <= sorter_[upper-1].pos[axis]) {
+        if (maxx >= mid_value) {
           out_starts_.emplace_back(mid);
         }
       }
@@ -99,9 +114,17 @@ private:
     sorter_t(glm::vec3 pos, size_t index) : pos(pos), index(index) {}
   };
 
-  std::vector<sorter_t> sorter_;
+  struct partition_t {
+    size_t lower;
+    size_t upper;
+    size_t mid;
+    float mid_value;
+  };
+
   std::vector<size_t> in_starts_;
   std::vector<size_t> out_starts_;
+  std::vector<partition_t> partitions_;
+  size_t size_;
 };
 
 int main() {
@@ -115,6 +138,9 @@ int main() {
     std::vector<float> radii = pdb.radii(chainID);
 
     kdtree kd(pos);
+
+    const std::vector <size_t> &res = kd.find_in_range(pos[0], pos[0]);
+    return 0;
 
     glm::vec3 min = pos[0];
     glm::vec3 max = pos[0];
